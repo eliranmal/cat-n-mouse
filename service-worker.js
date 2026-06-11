069d865d8df2a8b60e741f6809cf962f8102774a
lyn <lyn@redhat.com>
2026-06-11 13:17:29 +0300

    feat: add key command to switch cursor image

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,35 @@
 
-chrome.commands.onCommand.addListener(command => {
-  if (command === 'toggle-cursor') {
+
+const increment = (num, ceil) => {
+  const n = Number.parseInt(num, 10);
+  return n % ceil + 1;
+};
+
+const rotateCatName = (name) => (
+    name.replace(/-(\d+)$/, (_, strIndex) => (
+        `-${increment(strIndex, 25)}`)
+    )
+)
+
+const rotateCursor = async () => {
+  const currentData = await chrome.storage.local.get({
+    'cat-name': 'cat-1',
+  });
+  const nextCatName = rotateCatName(currentData['cat-name']);
+  return chrome.storage.local.set({
+    'cat-name': nextCatName,
+  });
+};
+
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === 'switch-cursor') {
+    await rotateCursor();
+  }
+
+  if (['toggle-cursor', 'switch-cursor'].includes(command)) {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => (
-      chrome.tabs.sendMessage(tabs[0].id, { command: 'toggle-cursor' })
+      chrome.tabs.sendMessage(tabs[0].id, { command })
     ));
   }
 });
